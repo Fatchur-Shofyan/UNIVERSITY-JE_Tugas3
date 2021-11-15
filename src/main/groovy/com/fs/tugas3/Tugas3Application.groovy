26922305d2e5a9a06c1263fbcd600d49cf82f8c6
Fatchur Shofyan <fatchur.shofyan@gmail.com>
2021-11-15 23:45:04 +0700

    initialization

---
+++ src/main/groovy/com/fs/tugas3/Tugas3Application.groovy
@@ -0,0 +1,13 @@
+package com.fs.tugas3
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Tugas3Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Tugas3Application, args)
+	}
+
+}
