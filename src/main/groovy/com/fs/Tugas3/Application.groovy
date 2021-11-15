6e0eb684fa7c79a2d7033b5798281deb862f3883
Fatchur Shofyan <fatchur.shofyan@gmail.com>
2021-11-16 01:22:35 +0700

    read

--- src/main/groovy/com/fs/Tugas3/Application.groovy
+++ src/main/groovy/com/fs/Tugas3/Application.groovy
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Tugas3Application {
+class Application {
 
 	static void main(String[] args) {
-		SpringApplication.run(Tugas3Application, args)
+		SpringApplication.run(Application, args)
 	}
 
 }
